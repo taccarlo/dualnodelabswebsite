637b8b42b8a7cafa4c83dc378505187c1a737652
Carlo Tacchella <tacchella.carlo@gmail.com>
2026-05-22 21:05:57 +0200

    Enable Server-Side Rendering (SSR) and hydration
    Configures the Angular application to utilize Server-Side Rendering (SSR) and client hydration. This improves initial page load performance, enhances SEO capabilities, and provides a better user experience.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, Routes } from '@angular/router';
 import { HomeComponent } from './home/home';
 import { PrivacyPolicyComponent } from './privacypolicy/privacypolicy';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'privacypolicy', component: PrivacyPolicyComponent }
+  { path: 'privacypolicy', component: PrivacyPolicyComponent },
 ];
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes)
-  ]
+  providers: [provideRouter(routes), provideClientHydration(withEventReplay())],
 };
