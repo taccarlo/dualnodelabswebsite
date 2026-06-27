0bc3fae3cee1cd8384935ae11bf032f877886ad8
Carlo Tacchella <tacchella.carlo@gmail.com>
2026-06-27 10:13:28 +0200

    this is breaking blitz, reverted

--- src/server.ts
+++ src/server.ts
@@ -39,10 +39,6 @@ app.use(
  * Handle all other requests by rendering the Angular application.
  */
 app.use((req, res, next) => {
-  if (req.path.startsWith('/exercises/patterns/')) {
-    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-  }
   angularApp
     .handle(req)
     .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
